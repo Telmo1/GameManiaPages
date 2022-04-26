2db3a8f8fada693f4f72c09f85a9bfec54c7213f
Telmo <yrtelmo00@gmail.com>
2022-04-25 21:53:41 -0300

    Modificaçoes jquerry e carrossel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,53 @@
-$(document).ready(function() {
-    $("#barras").elick(function)
-})
+// $(document).ready(function() {
 
+//     $("#fas").click(function) {
 
+//        if ($("#menu").hasclass("menu-ativo")) {
+//            $("#menu").removeClass("menu-ativo")
+//        } else {
+//            $("#menu").addClass("menu-ativo")
+//        }
+
+//     })
+// })
+
+// $(document).ready(function() {
+
+//     $("#fas").click(function()
+    
+//     $("#menu").toggleClass("menu-ativo")
+   
+//     ){
+
+//  })
+    
+// $(document).ready(function() {
+    
+//     let slideAtual = 1;
+//     let listaSlides = ["banner-dupla","banner-controle","banner-mario"]
+
+//     setInterval(mudarSlide, 1000)
+
+//     function mudarSlide (){
+//         console.log("slide atual",slideAtual);
+
+//         if (slideAtual > 0)
+//             $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+//     } else if (slideAtual == 0) {
+//         $("#carrossel").removeClass(listaSlides[(listaSlides.length) - 1 ])
+//     }
+
+//     $("#carrossel").addClass(listaSlides[slideAtual]);
+
+//     slideAtual++
+
+//     if(slideAtual > (listaSlides,length) - 1  ) {
+//         slideAtual = 0;
+//     }
+
+//     )}
+
+$('.carousel').carousel()
 
 function mostrarMenu() {
     let menu = document.getElementById("menu")
